c204ee8e063f1db313191fec440d4d6b9a6fc493
Robert Iagar <robert.iagar@hotmail.com>
2013-07-27 17:18:54 +0300

    First commit.

---
+++ PodcastR.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PodcastR.Web.MvcApplication" Language="C#" %>
---
+++ PodcastR.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace PodcastR.Web
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
