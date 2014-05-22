d7ae3d5e4986ab984179e5e886a143a53e70667b
Robert Iagar <robert.iagar@hotmail.com>
2014-05-23 01:57:31 +0300

    Added core entities to PodcastR Web Api.

---
+++ PodcastR.WebApi/Controllers/PodcastsController.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using PodcastR.WebApi.Core.Entities;
+using PodcastR.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Xml.Linq;
+
+namespace PodcastR.WebApi.Controllers
+{
+    [Authorize]
+    public class PodcastsController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        private ApplicationUserManager _userManager;
+
+        public PodcastsController()
+        {
+        }
+
+        public PodcastsController(ApplicationDbContext context, ApplicationUserManager manager)
+        {
+            DbContext = context;
+            UserManager = manager;
+        }
+
+        public ApplicationDbContext DbContext
+        {
+            get
+            {
+                return _dbContext ?? Request.GetOwinContext().Get<ApplicationDbContext>();
+            }
+            private set
+            {
+                _dbContext = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the current users Podcast.
+        /// </summary>
+        /// <returns>A collection of <see cref="Podcast">Podcasts</see>.</returns>
+        public async Task<IEnumerable<Podcast>> Get()
+        {
+            var user = (await UserManager.FindByIdAsync(User.Identity.GetUserId()));
+
+            return await DbContext.Podcasts.Where(p => p.User.Id == user.Id).ToListAsync();
+        }
+
+        public async Task<IHttpActionResult> Post(string podcastUrl)
+        {
+            if (string.IsNullOrEmpty(podcastUrl))
+            {
+                return BadRequest("PodcastUrl cannot be empty.");
+            }
+
+            var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            if (user == null)
+            {
+                return null;
+            }
+            try
+            {
+                var httpClient = new HttpClient();
+                var xml = await httpClient.GetStringAsync(podcastUrl);
+                var xmlDocument = XDocument.Parse(xml);
+
+                var channelTitle = xmlDocument.Element("rss").Element("channel").Element("title").Value;
+                XNamespace itunes = "http://www.itunes.com/dtds/podcast-1.0.dtd";
+                var elements = xmlDocument.Element("rss").Element("channel").Elements("item");
+                var description = xmlDocument.Element("rss").Element("channel").Element("description").Value;
+                var imageUrl = xmlDocument.Element("rss").Element("channel").Element(itunes + "image").Attribute("href").Value;
+                var author = xmlDocument.Element("rss").Element("channel").Element(itunes + "author").Value;
+
+                var podcast = new Podcast
+                {
+                    FeedUrl = podcastUrl,
+                    Name = channelTitle,
+                    ImageUrl = imageUrl,
+                    Author = author,
+                    Description = description,
+                    DateAdded = DateTime.Now,
+                };
+
+                var episodes = new List<Episode>();
+
+                foreach (var element in elements)
+                {
+                    var episode = new Episode(element, podcast);
+                    episodes.Add(episode);
+                }
+                podcast.Episodes = episodes.OrderByDescending(episode => episode.Published).ToList();
+
+                DbContext.Podcasts.Add(podcast);
+                var x = await DbContext.SaveChangesAsync();
+
+                user.Podcasts.Add(podcast);
+                var y = await UserManager.UpdateAsync(user);
+
+                if (x != 0 && y.Succeeded)
+                {
+                    return Json<Podcast>(podcast);
+                }
+                else
+                {
+                    return BadRequest("error saving podcast");
+                }
+            }
+            catch
+            {
+                return InternalServerError(new ApplicationException("Error processing podcast url."));
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                UserManager.Dispose();
+                DbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- PodcastR.WebApi/Models/IdentityModels.cs
+++ PodcastR.WebApi/Models/IdentityModels.cs
@@ -3,12 +3,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
+using PodcastR.WebApi.Core.Entities;
+using System.Collections.Generic;
+using System;
+using System.Runtime.Serialization;
 
 namespace PodcastR.WebApi.Models
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public ApplicationUser()
+        {
+            this.Podcasts = new List<Podcast>();
+        }
+
+        public virtual IList<Podcast> Podcasts { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -24,7 +36,10 @@ namespace PodcastR.WebApi.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
-        
+
+        public DbSet<Podcast> Podcasts { get; set; }
+        public DbSet<Episode> Episodes { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
